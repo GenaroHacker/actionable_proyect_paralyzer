4928ad7e458810d85c02c6030471d7737617a229
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2023-09-13 18:33:05 +0200

    Add files via upload

---
+++ two screens/a.py
@@ -0,0 +1,22 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.scrollview import ScrollView
+
+from kivy.uix.screenmanager import WipeTransition
+
+class ScreenA(Screen):
+    def __init__(self, **kwargs):
+        super(ScreenA, self).__init__(**kwargs)
+        layout = GridLayout(cols=1, padding=10, spacing=10, size_hint=(1, None), width=500)
+        layout.bind(minimum_height=layout.setter('height'))
+        btn = Button(text="Go to Screen B", size=(480, 40), size_hint=(1, None))
+        btn.bind(on_press=self.change_screen)
+        layout.add_widget(btn)
+        root = ScrollView(size_hint=(1, 1), size=(500, 320), pos_hint={'center_x': .5, 'center_y': .5}, do_scroll_x=False)
+        root.add_widget(layout)
+        self.add_widget(root)
+
+    def change_screen(self, instance):
+        self.manager.transition = WipeTransition()
+        self.manager.current = "screen_b"
