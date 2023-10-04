ca15802696e4faa5fb5b17fd3937593adc2d6656
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2023-10-04 20:21:40 +0200

    Update create_example_database.py

--- modules/create_example_database.py
+++ modules/create_example_database.py
@@ -1,173 +1,186 @@
-from actionable.modules.sql_core import create_table
-from actionable.modules.sql_core import insert_record
-from actionable.modules.sql_core import insert_several_records
-from actionable.modules.sql_core import read_records
-from actionable.modules.sql_core import read_last_record
-from actionable.modules.sql_core import update_record
-from actionable.modules.sql_core import remove_record
-from actionable.modules.sql_core import run_command
+from actionable.modules.sql_core import (
+    create_table, 
+    insert_record, 
+    insert_several_records, 
+    read_records,
+    read_last_record,
+    update_record,
+    remove_record,
+    run_command
+)
+
+DB_NAME = "time_management.db"
+
+def create_tables():
+    tables = {
+        "CONTEXTS": """
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            NAME TEXT,
+            EXPECTED_PERCENTAGE INTEGER,
+            FUTURE_EXPECTED_PERCENTAGE INTEGER,
+            R REAL,
+            G REAL,
+            B REAL
+        """,
+        "TAGS": """
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            NAME TEXT,
+            CONTEXT_ID INTEGER REFERENCES CONTEXTS(ID),
+            RANK INTEGER
+        """,
+        "TASKS": """
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            NAME TEXT,
+            TAG_ID INTEGER REFERENCES TAGS(ID),
+            RANK INTEGER,
+            DEPENDENCY_ID INTEGER REFERENCES TASKS(ID),
+            START TIME,
+            END TIME,
+            FREQUENCY REAL,
+            WEEKDAYS TEXT
+        """,
+        "SUBTASKS": """
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            TASK_ID INTEGER REFERENCES TASKS(ID),
+            ORDER_SEQUENCE INTEGER,
+            NAME TEXT,
+            DETAILS TEXT,
+            DURATION INTEGER
+        """,
+        "PLACEHOLDERS": """
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            TYPE TEXT,
+            VALUE TEXT
+        """,
+        "HISTORY": """
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            SUBTASK_ID INTEGER REFERENCES SUBTASKS(ID),
+            ACTION TEXT,
+            TIME DATETIME
+        """
+    }
+
+    for table_name, table_structure in tables.items():
+        create_table(DB_NAME, table_name, table_structure)
+
+def populate_contexts():
+    records = [
+        ("Create", 25, 25, 0.00, 0.45, 1.00),
+        ("Exercise", 25, 25, 1.00, 0.46, 0.00),
+        ("Recreate", 25, 25, 0.13, 0.64, 0.15),
+        ("Sleep", 25, 25, 0.46, 0.00, 0.00)
+    ]
+
+    for record in records:
+        insert_record(DB_NAME, "CONTEXTS", ["NAME", "EXPECTED_PERCENTAGE", "FUTURE_EXPECTED_PERCENTAGE", "R", "G", "B"], record)
+
+def populate_tags():
+    records = [
+        ("write", 1, 1),
+        ("design", 1, 2),
+        ("program", 1, 3),
+        ("brainstorm", 1, 4),
+        ("hit", 2, 1),
+        ("strengthen", 2, 2),
+        ("cardio", 2, 3),
+        ("sport", 2, 4),
+        ("read", 3, 1),
+        ("watch", 3, 2),
+        ("game", 3, 3),
+        ("outdoor", 3, 4),
+        ("sleep", 4, 1),
+        ("meditate", 4, 2)
+    ]
+
+    for record in records:
+        insert_record(DB_NAME, "TAGS", ["NAME", "CONTEXT_ID", "RANK"], record)
+
+def populate_tasks():
+    records = [
+        ("Write a thank you list", 1, 1, "yyyyyyy", None, "08:00:00", "20:00:00", 1),
+        ("Automate a repetitive computer task", 3, 1, "yyyyyyy", None, "08:00:00", "20:00:00", 1),
+        ("Learn a new %programming_language% concept", 3, 2, "yyyyyyy", None, "08:00:00", "20:00:00", 1),
+        ("Plan meals for the upcoming week", 4, 1, "yyyyyyy", None, "08:00:00", "20:00:00", 1),
+        ("Do a quick %exercise_name% session", 5, 1, "yyyyyyy", 6, "08:00:00", "20:00:00", 1.5),
+        ("Perform basic %exercise_name%", 6, 1, "yyyyyyy", 6, "08:00:00", "20:00:00", 1),
+        ("Watch %movie_name%", 9, 1, "yyyyyyy", None, "08:00:00", "20:00:00", 1),
+        ("Go to sleep", 13, 1, "yyyyyyy", None, "08:00:00", "20:00:00", 1)
+    ]
+
+    for record in records:
+        insert_record(DB_NAME, "TASKS", ["NAME", "TAG_ID", "RANK", "WEEKDAYS", "DEPENDENCY_ID", "START", "END", "FREQUENCY"], record)
+
+def populate_subtasks():
+    records = [
+        # Subtasks for Task 1 ('Write a thank you list')
+        (1, 1, "Choose person", None, 5),
+        (1, 2, "Write message", "Express gratitude sincerely", 10),
+
+        # Subtasks for Task 2 ('Automate a repetitive computer task')
+        (2, 1, "Identify task", None, 10),
+        (2, 2, "Write script for %programming_language%", "Research library or framework if needed", 60),
+        (2, 3, "Test script", None, 30),
+
+        # Subtasks for Task 3 ('Learn a new programming concept')
+        (3, 1, "Choose %programming_language% concept", None, 10),
+        (3, 2, "Read %programming_language% documentation", None, 30),
+        (3, 3, "Implement example in %programming_language%", None, 45),
+
+        # Subtasks for Task 4 ('Plan meals for the upcoming week')
+        (4, 1, "Choose recipes", None, 20),
+        (4, 2, "Make a grocery list", None, 15),
+        (4, 3, "Schedule cooking times", None, 10),
+
+        # Subtasks for Task 5 ('Do a quick workout session')
+        (5, 1, "Prepare for %exercise_name% session", "Gather equipment or clear space", 10),
+        (5, 2, "Perform %exercise_name%", None, 30),
+        (5, 3, "Workout", "Follow exercise routine", 20),
+
+        # Subtasks for Task 6 ('Perform basic push-ups, squats, and lunges')
+        (6, 1, "Warm up", None, 5),
+        (6, 2, "Perform %exercise_name%", "Keep good form", 20),
+        (6, 3, "Perform squats", None, 10),
+        (6, 4, "Perform lunges", None, 10),
+
+        # Subtasks for Task 7 ('Watch a recommended movie')
+        (7, 1, "Choose %movie_name%", None, 5),
+        (7, 2, "Set up watching space", "Comfortable seating, snacks", 10),
+        (7, 3, "Watch %movie_name%", None, 120),
+
+        # Subtasks for Task 8 ('Go to sleep')
+        (8, 1, "Prepare room", "Dim lights, quiet environment", 5),
+        (8, 2, "Brush teeth", None, 5),
+        (8, 3, "Lay in bed", None, 1)
+    ]
+
+    for record in records:
+        insert_record(DB_NAME, "SUBTASKS", ["TASK_ID", "ORDER_SEQUENCE", "NAME", "DETAILS", "DURATION"], record)
+
+def populate_placeholders():
+    records = [
+        ("programming_language", "Python"),
+        ("programming_language", "JavaScript"),
+        ("programming_language", "Java"),
+        ("programming_language", "C++"),
+
+        ("movie_name", "Inception"),
+        ("movie_name", "The Matrix"),
+        ("movie_name", "Interstellar"),
+
+        ("exercise_name", "push ups"),
+        ("exercise_name", "sit ups"),
+        ("exercise_name", "squats"),
+        ("exercise_name", "jumping jacks")
+    ]
+
+    for record in records:
+        insert_record(DB_NAME, "PLACEHOLDERS", ["TYPE", "VALUE"], record)
 
 def create_example_database():
-    # @title Create Tables
-    def create_tables():
-        # Define the structure for each table with the specified changes
-        tables = {
-            "CONTEXTS": """
-                ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                NAME TEXT,
-                EXPECTED_PERCENTAGE INTEGER,
-                FUTURE_EXPECTED_PERCENTAGE INTEGER,
-                R REAL,
-                G REAL,
-                B REAL
-            """,
-            "TAGS": """
-                ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                NAME TEXT,
-                CONTEXT_ID INTEGER REFERENCES CONTEXTS(ID),
-                RANK INTEGER
-            """,
-            "TASKS": """
-                ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                NAME TEXT,
-                TAG_ID INTEGER REFERENCES TAGS(ID),
-                RANK INTEGER,
-                DEPENDENCY_ID INTEGER REFERENCES TASKS(ID),
-                START TIME,
-                END TIME,
-                FREQUENCY REAL,
-                WEEKDAYS TEXT
-            """,
-            "SUBTASKS": """
-                ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                TASK_ID INTEGER REFERENCES TASKS(ID),
-                ORDER_SEQUENCE INTEGER,
-                NAME TEXT,
-                DETAILS TEXT,
-                DURATION INTEGER
-            """,
-            "PLACEHOLDERS": """
-                ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                TYPE TEXT,
-                VALUE TEXT
-            """,
-            "HISTORY": """
-                ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                SUBTASK_ID INTEGER REFERENCES SUBTASKS(ID),
-                ACTION TEXT,
-                TIME DATETIME
-            """
-        }
-    
-        # Create each table with the updated structure
-        for table_name, table_structure in tables.items():
-            create_table("time_management.db", table_name, table_structure)
-    
-    # @title Populate Example Records
-    def populate_tables():
-        # Populate the CONTEXTS table
-        contexts_records = [
-            "INSERT INTO CONTEXTS (NAME, EXPECTED_PERCENTAGE, FUTURE_EXPECTED_PERCENTAGE, R, G, B) VALUES ('Create', 25, 25, 0.00, 0.45, 1.00)",
-            "INSERT INTO CONTEXTS (NAME, EXPECTED_PERCENTAGE, FUTURE_EXPECTED_PERCENTAGE, R, G, B) VALUES ('Exercise', 25, 25, 1.00, 0.46, 0.00)",
-            "INSERT INTO CONTEXTS (NAME, EXPECTED_PERCENTAGE, FUTURE_EXPECTED_PERCENTAGE, R, G, B) VALUES ('Recreate', 25, 25, 0.13, 0.64, 0.15)",
-            "INSERT INTO CONTEXTS (NAME, EXPECTED_PERCENTAGE, FUTURE_EXPECTED_PERCENTAGE, R, G, B) VALUES ('Sleep', 25, 25, 0.46, 0.00, 0.00)"
-        ]
-        insert_several_records("time_management.db", contexts_records)
-    
-        # Populate the TAGS table
-        tags_records = [
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('write', 1, 1)",
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('design', 1, 2)",
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('program', 1, 3)",
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('brainstorm', 1, 4)",
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('hit', 2, 1)",
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('strengthen', 2, 2)",
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('cardio', 2, 3)",
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('sport', 2, 4)",
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('read', 3, 1)",
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('watch', 3, 2)",
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('game', 3, 3)",
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('outdoor', 3, 4)",
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('sleep', 4, 1)",
-            "INSERT INTO TAGS (NAME, CONTEXT_ID, RANK) VALUES ('meditate', 4, 2)"
-        ]
-        insert_several_records("time_management.db", tags_records)
-    
-        # Populate the TASKS table
-        tasks_records = [
-            "INSERT INTO TASKS (NAME, TAG_ID, RANK, WEEKDAYS, DEPENDENCY_ID, START, END, FREQUENCY) VALUES ('Write a thank you list', 1, 1, 'yyyyyyy', NULL, '08:00:00', '20:00:00', 1)",
-            "INSERT INTO TASKS (NAME, TAG_ID, RANK, WEEKDAYS, DEPENDENCY_ID, START, END, FREQUENCY) VALUES ('Automate a repetitive computer task', 3, 1, 'yyyyyyy', NULL, '08:00:00', '20:00:00', 1)",
-            "INSERT INTO TASKS (NAME, TAG_ID, RANK, WEEKDAYS, DEPENDENCY_ID, START, END, FREQUENCY) VALUES ('Learn a new %programming_language% concept', 3, 2, 'yyyyyyy', NULL, '08:00:00', '20:00:00', 1)",
-            "INSERT INTO TASKS (NAME, TAG_ID, RANK, WEEKDAYS, DEPENDENCY_ID, START, END, FREQUENCY) VALUES ('Plan meals for the upcoming week', 4, 1, 'yyyyyyy', NULL, '08:00:00', '20:00:00', 1)",
-            "INSERT INTO TASKS (NAME, TAG_ID, RANK, WEEKDAYS, DEPENDENCY_ID, START, END, FREQUENCY) VALUES ('Do a quick %exercise_name% session', 5, 1, 'yyyyyyy', NULL, '08:00:00', '20:00:00', 1.5)",
-            "INSERT INTO TASKS (NAME, TAG_ID, RANK, WEEKDAYS, DEPENDENCY_ID, START, END, FREQUENCY) VALUES ('Perform basic %exercise_name%', 6, 1, 'yyyyyyy', 6, '08:00:00', '20:00:00', 1)",
-            "INSERT INTO TASKS (NAME, TAG_ID, RANK, WEEKDAYS, DEPENDENCY_ID, START, END, FREQUENCY) VALUES ('Watch %movie_name%', 9, 1, 'yyyyyyy', NULL, '08:00:00', '20:00:00', 1)",
-            "INSERT INTO TASKS (NAME, TAG_ID, RANK, WEEKDAYS, DEPENDENCY_ID, START, END, FREQUENCY) VALUES ('Go to sleep', 13, 1, 'yyyyyyy', NULL, '08:00:00', '20:00:00', 1)"
-        ]
-        insert_several_records("time_management.db", tasks_records)
-    
-        subtasks_records = [
-            # Subtasks for Task 1 ('Write a thank you list')
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (1, 1, 'Choose person', NULL, 5)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (1, 2, 'Write message', 'Express gratitude sincerely', 10)",
-    
-            # Subtasks for Task 2 ('Automate a repetitive computer task')
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (2, 1, 'Identify task', NULL, 10)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (2, 2, 'Write script for %programming_language%', 'Research library or framework if needed', 60)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (2, 3, 'Test script', NULL, 30)",
-    
-            # Subtasks for Task 3 ('Learn a new programming concept')
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (3, 1, 'Choose %programming_language% concept', NULL, 10)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (3, 2, 'Read %programming_language% documentation', NULL, 30)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (3, 3, 'Implement example in %programming_language%', NULL, 45)",
-    
-            # Subtasks for Task 4 ('Plan meals for the upcoming week')
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (4, 1, 'Choose recipes', NULL, 20)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (4, 2, 'Make a grocery list', NULL, 15)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (4, 3, 'Schedule cooking times', NULL, 10)",
-    
-            # Subtasks for Task 5 ('Do a quick workout session')
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (5, 1, 'Prepare for %exercise_name% session', 'Gather equipment or clear space', 10)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (5, 2, 'Perform %exercise_name%', NULL, 30)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (5, 3, 'Workout', 'Follow exercise routine', 20)",
-    
-            # Subtasks for Task 6 ('Perform basic push-ups, squats, and lunges')
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (6, 1, 'Warm up', NULL, 5)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (6, 2, 'Perform %exercise_name%', 'Keep good form', 20)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (6, 3, 'Perform squats', NULL, 10)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (6, 4, 'Perform lunges', NULL, 10)",
-    
-            # Subtasks for Task 7 ('Watch a recommended movie')
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (7, 1, 'Choose %movie_name%', NULL, 5)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (7, 2, 'Set up watching space', 'Comfortable seating, snacks', 10)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (7, 3, 'Watch %movie_name%', NULL, 120)",
-    
-            # Subtasks for Task 8 ('Go to sleep')
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (8, 1, 'Prepare room', 'Dim lights, quiet environment', 5)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (8, 2, 'Brush teeth', NULL, 5)",
-            "INSERT INTO SUBTASKS (TASK_ID, ORDER_SEQUENCE, NAME, DETAILS, DURATION) VALUES (8, 3, 'Lay in bed', NULL, 1)"
-        ]
-    
-        insert_several_records("time_management.db", subtasks_records)
-    
-        placeholders_records = [
-            "INSERT INTO PLACEHOLDERS (TYPE, VALUE) VALUES ('programming_language', 'Python')",
-            "INSERT INTO PLACEHOLDERS (TYPE, VALUE) VALUES ('programming_language', 'JavaScript')",
-            "INSERT INTO PLACEHOLDERS (TYPE, VALUE) VALUES ('programming_language', 'Java')",
-            "INSERT INTO PLACEHOLDERS (TYPE, VALUE) VALUES ('programming_language', 'C++')",
-    
-            "INSERT INTO PLACEHOLDERS (TYPE, VALUE) VALUES ('movie_name', 'Inception')",
-            "INSERT INTO PLACEHOLDERS (TYPE, VALUE) VALUES ('movie_name', 'The Matrix')",
-            "INSERT INTO PLACEHOLDERS (TYPE, VALUE) VALUES ('movie_name', 'Interstellar')",
-    
-            "INSERT INTO PLACEHOLDERS (TYPE, VALUE) VALUES ('exercise_name', 'push ups')",
-            "INSERT INTO PLACEHOLDERS (TYPE, VALUE) VALUES ('exercise_name', 'sit ups')",
-            "INSERT INTO PLACEHOLDERS (TYPE, VALUE) VALUES ('exercise_name', 'squats')",
-            "INSERT INTO PLACEHOLDERS (TYPE, VALUE) VALUES ('exercise_name', 'jumping jacks')"
-        ]
-        insert_several_records("time_management.db", placeholders_records)
-
     create_tables()
-    populate_tables()
-    
-create_example_database()
+    populate_contexts()
+    populate_tags()
+    populate_tasks()
+    populate_subtasks()
+    populate_placeholders()
+
