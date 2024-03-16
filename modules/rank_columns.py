3146e407a1290ee93433a956cc23f98b31590686
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-03-16 10:35:57 +0100

    Update rank_columns.py

--- modules/rank_columns.py
+++ modules/rank_columns.py
@@ -5,7 +5,7 @@ import re
 def ask_user_cmp(item1, item2):
     while True:
         print(f" [ 1 ] [{item1}] ?" )
-        print(f" [ 2 ] [{item2}] ?")
+        print(f" [ 2 ] [{item2}] ?") 
         cmp = input(" --> ? ")
         if cmp == "1":
             return 1
